78dae4c307da6e2f3a71348c3325fe791db5ef50
Tobias Theuer <tobias.theuer@gmx.de>
2023-12-05 13:55:46 +0100

    Fix ep square being updated twice in non-nullmove
    bench: 9843717

--- src/games/chess.rs
+++ src/games/chess.rs
@@ -251,6 +251,10 @@ impl Board for Chessboard {
     fn make_nullmove(mut self) -> Option<Self> {
         self.ply += 1;
         self.ply_100_ctr += 1;
+        if self.ep_square.is_some() {
+            self.hash ^= PRECOMPUTED_ZOBRIST_KEYS.ep_file_keys[self.ep_square.unwrap().file()];
+            self.ep_square = None;
+        }
         self.flip_side_to_move()
     }
 
