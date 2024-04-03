5fc3d70cf614de71e32379ab71de08469b0617dc
Tobias Theuer <tobias.theuer@gmx.de>
2024-04-03 16:57:08 +0200

    fix zobrist hashing for chess

--- gears/src/games/chess/zobrist.rs
+++ gears/src/games/chess/zobrist.rs
@@ -1,10 +1,10 @@
 use strum::IntoEnumIterator;
 
+use crate::games::{Color, ColoredPieceType, ZobristHash};
+use crate::games::chess::Chessboard;
 use crate::games::chess::pieces::{ColoredChessPiece, UncoloredChessPiece};
 use crate::games::chess::squares::{ChessSquare, NUM_COLUMNS};
-use crate::games::chess::Chessboard;
 use crate::games::Color::*;
-use crate::games::{Color, ColoredPieceType, ZobristHash};
 use crate::general::bitboards::Bitboard;
 
 pub const NUM_PIECE_SQUARE_ENTRIES: usize = 64 * 6;
@@ -24,7 +24,7 @@ impl PrecomputedZobristKeys {
         color: Color,
         square: ChessSquare,
     ) -> ZobristHash {
-        self.piece_square_keys[square.index() + piece as usize * 2 + color as usize]
+        self.piece_square_keys[square.index() * 12 + piece as usize * 2 + color as usize]
     }
 }
 
@@ -38,9 +38,14 @@ const INCREMENT: u128 = (6364136223846793005 << 64) + 1442695040888963407;
 // the pcg xsl rr 128 64 oneseq generator, aka pcg64_oneseq (most other pcg generators have additional problems)
 impl PcgXslRr128_64Oneseq {
     const fn new(seed: u128) -> Self {
-        Self(seed + INCREMENT)
+        Self(
+            seed.wrapping_add(INCREMENT)
+                .wrapping_mul(MUTLIPLIER)
+                .wrapping_add(INCREMENT),
+        )
     }
 
+    // const mut refs aren't stable yet, so returning the new state is a workaround
     const fn gen(mut self) -> (Self, ZobristHash) {
         self.0 = self.0.wrapping_mul(MUTLIPLIER);
         self.0 = self.0.wrapping_add(INCREMENT);
@@ -139,10 +144,124 @@ impl Chessboard {
 
 #[cfg(test)]
 mod tests {
-    use crate::games::chess::moves::{ChessMove, ChessMoveFlags};
-    use crate::games::chess::squares::{ChessSquare, D_FILE_NO, E_FILE_NO};
+    use std::collections::{HashMap, VecDeque};
+
+    use itertools::Itertools;
+
+    use crate::games::{Board, ZobristHash};
     use crate::games::chess::Chessboard;
-    use crate::games::Board;
+    use crate::games::chess::moves::{ChessMove, ChessMoveFlags};
+    use crate::games::chess::pieces::UncoloredChessPiece::{Bishop, Knight};
+    use crate::games::chess::squares::{ChessSquare, D_FILE_NO, E_FILE_NO};
+    use crate::games::chess::zobrist::{PcgXslRr128_64Oneseq, PRECOMPUTED_ZOBRIST_KEYS};
+    use crate::games::Color::{Black, White};
+
+    #[test]
+    fn pcg_test() {
+        let gen = PcgXslRr128_64Oneseq::new(42);
+        assert_eq!(gen.0 >> 64, 1610214578838163691);
+        assert_eq!(gen.0 & ((1 << 64) - 1), 13841303961814150380);
+        let (gen, rand) = gen.gen();
+        assert_eq!(rand.0, 2915081201720324186);
+        let (gen, rand) = gen.gen();
+        assert_eq!(rand.0, 13533757442135995717);
+        let (gen, rand) = gen.gen();
+        assert_eq!(rand.0, 13172715927431628928);
+    }
+
+    #[test]
+    fn simple_test() {
+        let a1 = PRECOMPUTED_ZOBRIST_KEYS
+            .piece_key(Bishop, White, ChessSquare::from_chars('f', '4').unwrap())
+            .0;
+        let b1 = PRECOMPUTED_ZOBRIST_KEYS
+            .piece_key(Bishop, White, ChessSquare::from_chars('g', '5').unwrap())
+            .0;
+        let a2 = PRECOMPUTED_ZOBRIST_KEYS
+            .piece_key(Knight, Black, ChessSquare::from_chars('h', '5').unwrap())
+            .0;
+        let b2 = PRECOMPUTED_ZOBRIST_KEYS
+            .piece_key(Knight, Black, ChessSquare::from_chars('g', '4').unwrap())
+            .0;
+        println!("{a1} {a2} {b1} {b2}");
+        assert_ne!(a1 ^ a2, b1 ^ b2); // used to be bugged
+        let position = Chessboard::from_name("kiwipete").unwrap();
+        println!("{}", position.as_fen());
+        let hash = position.hash;
+        let mut hashes = HashMap::new();
+        let mut collisions = HashMap::new();
+        for mov in position.legal_moves_slow() {
+            let new_board = position.make_move(mov).unwrap();
+            assert_ne!(new_board.hash, hash);
+            let previous = hashes.insert(new_board.hash.0, new_board);
+            assert!(previous.is_none());
+            let different_bits = (new_board.hash.0 ^ hash.0).count_ones();
+            assert!(different_bits >= 16 && different_bits <= 48);
+            for mov in new_board.legal_moves_slow() {
+                let new_board = new_board.make_move(mov).unwrap();
+                let previous = hashes.insert(new_board.hash.0, new_board);
+                if previous.is_some() {
+                    let old_board = previous.unwrap();
+                    println!(
+                        "Collision at hash {hash}, boards {0} and {1} (diagrams: \n{old_board} and \n{new_board}",
+                        old_board.as_fen(),
+                        new_board.as_fen()
+                    );
+                    // There's one ep move after one ply from the current position, which creates the only transposition reachable within 2 plies
+                    if old_board != new_board {
+                        collisions.insert(new_board.hash.0, [old_board, new_board]);
+                    }
+                }
+                let different_bits = (new_board.hash.0 ^ hash.0).count_ones();
+                println!("{different_bits}");
+                assert!(different_bits >= 12 && different_bits < 52);
+            }
+        }
+        assert!(
+            collisions.is_empty(),
+            "num collisions: {0} out of {1}",
+            collisions.len(),
+            hashes.len()
+        );
+    }
+
+    #[test]
+    fn statistical_test() {
+        let position = Chessboard::default();
+        let mut hashes = Vec::new();
+        let mut queue = VecDeque::new();
+        queue.push_back(position);
+        let max_queue_len = if cfg!(debug_assertions) {
+            1_000_000
+        } else {
+            10_000_000
+        };
+        while queue.len() <= max_queue_len {
+            let pos = queue.front().copied().unwrap();
+            let moves = pos.legal_moves_slow();
+            queue.pop_front();
+            hashes.push(pos.hash);
+            for mov in moves {
+                queue.push_back(pos.make_move(mov).unwrap());
+            }
+        }
+        for entry in queue {
+            hashes.push(entry.hash);
+        }
+        hashes.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+        for shift in 0..64 - 8 {
+            let get_bits = |hash: ZobristHash| (hash.0 >> shift) & 0xff;
+            let mut counts = vec![0; 256];
+            for hash in hashes.iter() {
+                counts[get_bits(*hash) as usize] += 1;
+            }
+            let expected = hashes.len() / 256;
+            for count in counts {
+                assert!(count >= expected / 2);
+                assert!(count <= expected + expected / 2);
+            }
+        }
+    }
 
     #[test]
     fn ep_test() {
