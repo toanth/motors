503dd15ca8a68c1369bb51b7ca1bcf138dc0d8d7
Tobias Theuer <tobias.theuer@gmx.de>
2024-04-20 20:03:37 +0200

    remove unused human.rs file and fix the range of scores returned by rand_eval. bench: 4033575

--- motors/src/eval/rand_eval.rs
+++ motors/src/eval/rand_eval.rs
@@ -1,14 +1,15 @@
-use rand::{thread_rng, Rng};
+use rand::{Rng, thread_rng};
+
+use gears::games::Board;
+use gears::search::{MAX_NORMAL_SCORE, MIN_NORMAL_SCORE, Score};
 
 use crate::eval::Eval;
-use gears::games::Board;
-use gears::search::{Score, SCORE_LOST, SCORE_WON};
 
 #[derive(Debug, Default)]
 pub struct RandEval {}
 
 impl<B: Board> Eval<B> for RandEval {
     fn eval(&self, _: B) -> Score {
-        Score(thread_rng().gen_range(SCORE_LOST.0 + 1..SCORE_WON.0))
+        Score(thread_rng().gen_range(MIN_NORMAL_SCORE.0..MAX_NORMAL_SCORE.0))
     }
 }
