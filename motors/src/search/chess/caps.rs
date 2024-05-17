3ce353b9b2e58b7af1e0a55c954e9bc87ecbd434
Tobias Theuer <tobias.theuer@gmx.de>
2024-05-17 22:57:57 +0200

    Simplification: Increase min nmp depth to 4, no matter if regressing. bench: 6049277

--- motors/src/search/chess/caps.rs
+++ motors/src/search/chess/caps.rs
@@ -444,7 +444,7 @@ impl<E: Eval<Chessboard>> Caps<E> {
             // A more careful implementation would do a verification search to check for zugzwang, and possibly avoid even trying
             // nmp in a position with no pieces except the king and pawns.
             // TODO: Verification search.
-            if depth >= 3 + regressing as isize && eval >= beta {
+            if depth >= 4 && eval >= beta {
                 self.state.board_history.push(&pos);
                 let new_pos = pos.make_nullmove().unwrap();
                 let reduction = 3 + depth / 4;
