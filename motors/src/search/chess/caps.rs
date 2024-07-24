3078b00c7db4bef2e883c2ef650371fbbe2981d4
Tobias Theuer <tobias.theuer@gmx.de>
2024-07-24 17:42:15 +0200

    don't qsearch see prune equal captures. bench: 5257415, hash: 771D9BD938EFB6D2

--- motors/src/search/chess/caps.rs
+++ motors/src/search/chess/caps.rs
@@ -1053,7 +1053,7 @@ impl Caps {
         let mut children_visited = 0;
         while let Some((mov, score)) = move_picker.next(&move_scorer, &self.state) {
             debug_assert!(mov.is_tactical(&pos));
-            if score < MoveScore(0) {
+            if score < MoveScore(-HIST_DIVISOR) {
                 // qsearch see pruning: If the move has a negative SEE score, don't even bother playing it in qsearch.
                 break;
             }
