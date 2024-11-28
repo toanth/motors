5a5a03093f113ee26f36c5c2c44e3fddbd9c8f2b
Tobias Theuer <tobias.theuer@gmx.de>
2024-11-28 02:27:02 +0100

    tweak outpost definition. bench: 6666896, hash: 978A8611EE66D8B0

--- motors/src/eval/chess/lite.rs
+++ motors/src/eval/chess/lite.rs
@@ -211,16 +211,13 @@ impl<Tuned: LiteValues> GenericLiTEval<Tuned> {
 
     fn outposts(pos: &Chessboard, color: ChessColor) -> Tuned::Score {
         let mut score = Tuned::Score::default();
-        let our_pawns = pos.colored_piece_bb(color, Pawn);
-        let pawn_protection = our_pawns.pawn_attacks(color);
+        let their_pawns = pos.colored_piece_bb(!color, Pawn);
         for piece in ChessPieceType::non_pawn_pieces() {
             for square in pos.colored_piece_bb(color, piece).ones() {
                 let in_front = ((A_FILE << (square.flip_if(color == Black).bb_idx())) << 8)
                     .flip_if(color == Black);
                 let potential_attackers = in_front.west() | in_front.east();
-                if pawn_protection.is_bit_set_at(square.bb_idx())
-                    && (potential_attackers & pos.colored_piece_bb(!color, Pawn)).is_zero()
-                {
+                if (potential_attackers & their_pawns).is_zero() {
                     score += Tuned::outpost(piece);
                 }
             }
