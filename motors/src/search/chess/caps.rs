e83c768f1e642bba4ca910cdd5b119c6cd31eda4
Tobias Theuer <tobias.theuer@gmx.de>
2024-12-24 16:25:08 +0100

    if the TT move is a capture, futility prune more eagerly. bench: 6387294, hash: BD6E3333D2C1C7CD

--- motors/src/search/chess/caps.rs
+++ motors/src/search/chess/caps.rs
@@ -912,11 +912,14 @@ impl Caps {
             // FP (Futility Pruning): If the static eval is far below alpha,
             // then it's unlikely that a quiet move can raise alpha: We've probably blundered at some prior point in search,
             // so cut our losses and return. This has the potential of missing sacrificing mate combinations, though.
-            let fp_margin = if we_blundered {
+            let mut fp_margin = if we_blundered {
                 cc::fp_blunder_base() + cc::fp_blunder_scale() * depth
             } else {
                 cc::fp_base() + cc::fp_scale() * depth
             };
+            if is_noisy {
+                fp_margin /= 2;
+            }
             let mut lmp_threshold = if we_blundered {
                 cc::lmp_blunder_base() + cc::lmp_blunder_scale() * depth
             } else {
