2735879c4c1d6f7be19ea4c197ca150269fa2598
Tobias Theuer <tobias.theuer@gmx.de>
2025-04-17 19:59:10 +0200

    dont stop all qsearch move loop pruning based on eval, use best_score instead. bench: 7625355, hash: A149CF3C1BC2B58D

--- motors/src/search/chess/caps.rs
+++ motors/src/search/chess/caps.rs
@@ -1180,7 +1180,7 @@ impl Caps {
         let mut children_visited = 0;
         while let Some((mov, score)) = move_picker.next(&move_scorer, &self.state) {
             debug_assert!(mov.is_tactical(&pos) || pos.is_in_check());
-            if !eval.is_game_lost_score() && score < MoveScore(0) || children_visited >= 3 {
+            if !best_score.is_game_lost_score() && score < MoveScore(0) || children_visited >= 3 {
                 // qsearch see pruning and qsearch late move  pruning (lmp):
                 // If the move has a negative SEE score or if we've already looked at enough moves, don't even bother playing it in qsearch.
                 break;
