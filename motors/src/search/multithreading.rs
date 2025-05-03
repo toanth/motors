6fd39ab91929655cc48caca2053174f686bbf1ae
Tobias Theuer <tobias.theuer@gmx.de>
2025-05-03 20:31:23 +0200

    count_node() returns the new number of nodes, not the old number. bench: 7680330, hash: 15B1C08E0959238B

--- motors/src/search/multithreading.rs
+++ motors/src/search/multithreading.rs
@@ -242,7 +242,7 @@ impl<B: Board> AtomicSearchState<B> {
     pub(super) fn count_node(&self) -> u64 {
         // TODO: Test if using a relaxed load, non-atomic add, and relaxed store is faster
         // (should compile to `add` instead of `lock add` on x86)
-        self.nodes.fetch_add(1, Relaxed)
+        self.nodes.fetch_add(1, Relaxed) + 1
     }
 
     pub(super) fn set_depth(&self, depth: isize) {
