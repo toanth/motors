7a66771f12ef1ba302c9e9c05522b0dfa39032ef
Tobias Theuer <tobias.theuer@gmx.de>
2026-05-15 02:49:52 +0200

    fix. bench: 6937226, hash: FB377F316BC81D1

--- motors/src/search/chess/caps_values.rs
+++ motors/src/search/chess/caps_values.rs
@@ -47,9 +47,9 @@ spsa_params![
     rfr_reduction: isize = 256; 64..=512; step=32;
     iir_min_depth: isize = 512; 128..=32_768; step=32;
     iir_reduction: isize = 128; 0..=256; step=16;
-    hindsight_min_depth: isize = 384; 0..=2048; step = 64;
+    hindsight_min_depth: isize = 128; 0..=2048; step = 64;
     hindsight_min_reduction: isize = 512; 0..=1024; step = 32;
-    hindsight_eval_diff: ScoreT = 20; -64..=256; step = 32;
+    hindsight_eval_diff: ScoreT = -16; -256..=64; step = 16;
     hindsight_lmr: isize = 128; 0..=512; step=15;
     rfp_base: ScoreT = 158; 0..=900; step=15;
     rfp_blunder: ScoreT = 48; 0..=512; step=8;
